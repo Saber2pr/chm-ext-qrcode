65ce4b25340fa6e32ecfe3ad36aee436d92a8171
muhai <muhai.li@tuya.com>
2025-07-15 10:40:24 +0800

    chore: update

--- src/background.js
+++ src/background.js
@@ -1,23 +1,28 @@
-const COM_QRCODE = 'Qrcode Edit'
-const COM_QUERY = 'querySelector'
-const COM_SAVE = 'Save as file'
+// src/background.js (Manifest V3)
 
-const menus = [
-  COM_QRCODE,
-  COM_QUERY,
-  COM_SAVE
-]
+const COM_QRCODE = "Qrcode Edit";
+const COM_QUERY = "querySelector";
+const COM_SAVE = "Save as file";
 
+const menus = [COM_QRCODE, COM_QUERY, COM_SAVE];
+
+// 安装或更新时创建菜单
 chrome.runtime.onInstalled.addListener(() => {
-  menus.forEach(menu => chrome.contextMenus.create({
-    id: menu,
-    title: menu,
-    contexts: ['all']
-  }))
-})
+  menus.forEach((menu) => {
+    chrome.contextMenus.create({
+      id: menu,
+      title: menu,
+      contexts: ["all"],
+    });
+  });
+});
 
-chrome.contextMenus.onClicked.addListener((info) => {
-  if(menus.includes(info.menuItemId)){
-    sendMessage(info)
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (menus.includes(info.menuItemId)) {
+    chrome.tabs.sendMessage(tab.id, {
+      menuItemId: info.menuItemId, // 👈 保证内容脚本能拿到
+      type: info.menuItemId,
+      data: info,
+    });
   }
-})
+});
--- src/content.js
+++ src/content.js
@@ -1,114 +1,115 @@
-let x = 0
-let y = 0
+let x = 0;
+let y = 0;
 
-window.addEventListener('contextmenu', event => {
-  x = event.clientX
-  y = event.clientY
-})
+window.addEventListener("contextmenu", (event) => {
+  x = event.clientX;
+  y = event.clientY;
+});
 
-window.addEventListener('error', event => {
-  const filename = event.filename
-  if (filename.startsWith('chrome-extension://')) {
-    alert(event.error)
+window.addEventListener("error", (event) => {
+  const filename = event.filename;
+  if (filename.startsWith("chrome-extension://")) {
+    alert(event.error);
   }
-})
+});
 
 /**
  * @returns {HTMLElement}
  */
-const getClickElement = () => document.elementFromPoint(x, y)
+const getClickElement = () => document.elementFromPoint(x, y);
 
-let qrcode = null
-let tempQuery = ''
+let qrcode = null;
+let tempQuery = "";
 
-const isImg = element => element && element.tagName.toLowerCase() === 'img'
-const isCanvas = element => element && element.tagName.toLowerCase() === 'canvas'
+const isImg = (element) => element && element.tagName.toLowerCase() === "img";
+const isCanvas = (element) =>
+  element && element.tagName.toLowerCase() === "canvas";
 
 /**
- * @param {HTMLElement} element 
+ * @param {HTMLElement} element
  */
 const getElementImageBase64 = (element) => {
   if (element) {
     if (isCanvas(element)) {
-      return element.toDataURL('image/png')
+      return element.toDataURL("image/png");
     } else if (isImg(element)) {
-      return element.src
+      return element.src;
     } else {
-      const backgroundImage = element.style.backgroundImage
+      const backgroundImage = element.style.backgroundImage;
       if (backgroundImage) {
-        return backgroundImage.replace(/^url\("/, '').replace(/"\)$/, '')
+        return backgroundImage.replace(/^url\("/, "").replace(/"\)$/, "");
       }
     }
   }
-}
+};
 
 /**
- * @param {HTMLElement} element 
+ * @param {HTMLElement} element
  */
 const editQrcodeElement = (element) => {
-  const base64 = getElementImageBase64(element)
+  const base64 = getElementImageBase64(element);
   if (base64) {
-    qrcodeParser(base64).then(res => {
-      const url = res.data
-      const newUrl = window.prompt('Edit Qrcode Value:', url)
+    qrcodeParser(base64).then((res) => {
+      const url = res.data;
+      const newUrl = window.prompt("Edit Qrcode Value:", url);
       if (newUrl) {
         /** @type {HTMLCanvasElement} **/
         const canvas = new AraleQRCode({
           text: newUrl,
           width: element.clientWidth,
           height: element.clientHeight,
-          render: 'canvas',
-        })
-        const newBase64 = canvas.toDataURL('image/png')
+          render: "canvas",
+        });
+        const newBase64 = canvas.toDataURL("image/png");
         if (isImg(element)) {
-          element.src = newBase64
+          element.src = newBase64;
         } else if (isCanvas(element)) {
           // copy attrs
-          const attrs = element.getAttributeNames()
+          const attrs = element.getAttributeNames();
           if (attrs && attrs.length) {
-            attrs.forEach(key => {
-              if (!['width', 'height'].includes(key)) {
-                canvas.setAttribute(key, element.getAttribute(key))
+            attrs.forEach((key) => {
+              if (!["width", "height"].includes(key)) {
+                canvas.setAttribute(key, element.getAttribute(key));
               }
-            })
+            });
           }
-          element.parentElement.replaceChild(canvas, element)
+          element.parentElement.replaceChild(canvas, element);
         } else {
-          element.style.backgroundImage = `url(${newBase64})`
+          element.style.backgroundImage = `url(${newBase64})`;
         }
       }
-    })
+    });
   }
-}
+};
 
-const COM_QRCODE = 'Qrcode Edit'
-const COM_QUERY = 'querySelector'
-const COM_SAVE = 'Save as file'
+const COM_QRCODE = "Qrcode Edit";
+const COM_QUERY = "querySelector";
+const COM_SAVE = "Save as file";
 
-onMessage(info => {
+onMessage((info) => {
   if (info.menuItemId === COM_QRCODE) {
-    editQrcodeElement(getClickElement())
+    editQrcodeElement(getClickElement());
   }
   if (info.menuItemId === COM_QUERY) {
-    const selector = window.prompt('Query a qrcode element:', tempQuery)
+    const selector = window.prompt("Query a qrcode element:", tempQuery);
     if (selector) {
-      const element = document.querySelector(selector)
+      const element = document.querySelector(selector);
       if (element) {
-        tempQuery = selector
-        editQrcodeElement(element)
+        tempQuery = selector;
+        editQrcodeElement(element);
       } else {
-        alert('cannot found an element by the query!')
+        alert("cannot found an element by the query!");
       }
     }
   }
   if (info.menuItemId === COM_SAVE) {
-    const element = getClickElement()
-    const source = getElementImageBase64(element)
+    const element = getClickElement();
+    const source = getElementImageBase64(element);
     if (element && source) {
-      const a = document.createElement('a')
-      a.href = source
-      a.download = `qrcode-${Date.now()}.png`
-      a.click()
+      const a = document.createElement("a");
+      a.href = source;
+      a.download = `qrcode-${Date.now()}.png`;
+      a.click();
     }
   }
-})
+});
